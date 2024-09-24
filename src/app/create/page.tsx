d00522639da8958dbbe34a3f2fcdef61cc8c0a88
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-24 07:47:38 +0300

    Add CreatePage component for rendering a page with a coin creation form

---
+++ src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import CoinCreationForm from '@/components/create/coin-creation-form'
+import React from 'react'
+
+const CreatePage = () => {
+    return (
+        <div>
+            <CoinCreationForm />
+        </div>
+    )
+}
+
+export default CreatePage
