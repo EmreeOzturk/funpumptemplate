982a84e1ebb6b2d1cf80eddd506c72955f01f5f8
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-25 07:44:50 +0300

    Refactor layout component to include web3 provider and handle cookie for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import { Poppins } from "next/font/google"
 import Header from "@/components/header";
-
+import Providers from "@/components/providers/web3-provider";
+import { headers } from "next/headers";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -19,15 +21,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookie = headers().get("cookie");
   return (
-    <html lang="en">
+    <html lang="en" className="px-4 sm:px-6 lg:px-8 py-8   max-w-[1444px] mx-auto  ">
       <body
-        className={`antialiased ${poppins.className} max-w-[1444px] mx-auto px-4 sm:px-6 lg:px-8 py-8
-          bg-neutral-700 `}
-      >
-        <div className="absolute inset-0 bg-fuchsia-800 bg-[size:20px_20px] -z-10 opacity-20 blur-[100px]"></div>
-        <Header />
-        {children}
+        className={`antialiased ${poppins.className} bg-neutral-700 `}>
+        <Providers cookie={cookie}>
+          <div className="absolute inset-0 bg-fuchsia-800 bg-[size:20px_20px] -z-10 opacity-20 blur-[100px]"></div>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
